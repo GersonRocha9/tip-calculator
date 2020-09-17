9e270f63d8a118a41e3dbacb831a1d45764afba1
Gerson Rocha <gersonrocha9@gmail.com>
2020-09-17 12:35:39 -0300

    master

--- scripts.js
+++ scripts.js
@@ -1,11 +1,27 @@
-function billCalculator () {
-  var bill = document.getElementById('bill').value
-  let quality = document.getElementById('quality').value;
-  let people = document.getElementById('people').value;
-  let result = (bill*quality)/people;  
+function billCalculator (event) {
+  event.preventDefault();
+
+  let valueBill = document.getElementById('bill').value
+  let serviceQuality = document.getElementById('quality').value;
+  let manyPeople = document.getElementById('people').value;
+
+  if(manyPeople == "" | manyPeople <= 1) {
+    manyPeople = 1;
+    document.getElementById('each').style.display = "none"
+  }
+
+  else {
+    document.getElementById('each').style.display = "block"
+  }
+
+  let result = (valueBill*serviceQuality)/manyPeople; 
+  result = result.toFixed(2);
+
+  document.getElementById('tip').innerHTML = result;
+
 }
 
-function changeLabel() {
-  document.getElementById('result').innerHTML =
-  "<label for='total'>"+"</label> ";
-}
+document.getElementById('totalTip').style.display = "none"
+document.getElementById('each').style.display = "none"
+
+document.getElementById('tipsForm').addEventListener('submit', billCalculator)
