8aed3054d4881503b634729816295ace28462164
Gerson Rocha <gersonrocha9@gmail.com>
2020-09-17 11:19:15 -0300

    Construção do JavaScript

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function billCalculator () {
+  var bill = document.getElementById('bill').value
+  let quality = document.getElementById('quality').value;
+  let people = document.getElementById('people').value;
+  let result = (bill*quality)/people;
   
+}
+
+function changeLabel(text) {
+  document.getElementById('result').innerHTML =
+  "<label for='total'>"+text+"</label> ";
 }
